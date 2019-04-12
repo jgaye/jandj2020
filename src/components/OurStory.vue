55e5178cf440139578703098f7fb8aa1aa95160d
Julien Gaye <julien.gaye@gmail.com>
2019-04-12 21:23:44 +0200

    Text changes, and adding the wedding hoto album

--- src/components/OurStory.vue
+++ src/components/OurStory.vue
@@ -14,15 +14,15 @@
             We fell in love.
           </label>
           <label>
-            <img src="../assets/wedding_placeholder.png"><br>
+            <img src="../assets/wedding.png"><br>
             We got married!
           </label>
         </div>
 
         <div>
-        <p>We first met in the summer of 2016 working at the same Philadelphia-based startup. Julien was in the US on a six-month work visa, and Julia was hired following her university graduation. We quickly struck up a friendship, and it wasn’t long before Julien admitted his feelings for Julia, and (eventually) she admitted the same.</p>
+        <p>Julia and Julien first met in the summer of 2016 working at the same Philadelphia-based startup. Julien was in the US on a six-month work visa, and Julia was hired following her university graduation. They quickly struck up a friendship, and it wasn’t long before Julien admitted his feelings for Julia, and (eventually) she admitted the same.</p>
 
-        <p>We were inseparable until Julien’s return to France, but the distance couldn’t keep us apart. Eight months later, Julia moved to Paris and we started to build our life together. On March 30, 2019, we legally wed during a civil ceremony in Croissy-sur-Seine. We are excited to celebrate our love and commitment with all of our family and friends from both sides of the Atlantic in 2020.</p>
+        <p>The two were inseparable until Julien’s return to France, but the distance couldn’t keep them apart. Eight months later, Julia moved to Paris and they started to build their life together. On March 30, 2019, they legally wed during a civil ceremony in Croissy-sur-Seine. Julia and Julien are excited to celebrate their love and commitment with all of their family and friends from both sides of the Atlantic in 2020.</p>
         </div>
 
         </div>
--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -2,7 +2,70 @@
   <div class="content">
     <div>
       <h2>Photos</h2>
-      <p>Enjoy photos from our engagement photo shoot in Montmartre at the Sacré-Cœur, by <a href="https://www.missparisphoto.com/">Krystal Kenny</a>.</p>
+      <p>Enjoy photos from our civil ceremony in Croissy-sur-Seine, courtesy of Ophélie, sister-in-law of the groom.</p>
+    </div>
+
+    <div class="pa-embed-player" style="width:100%; height:480px; display:none;"
+      data-link="https://photos.app.goo.gl/uU7iWnX4F8YXHSVU7"
+      data-title="J and J 2019"
+      data-description="50 new photos · Album by Julien Gaye">
+      <img data-src="https://lh3.googleusercontent.com/VxdrtIXtFUt4ikbkZS4_aNbxDvCyWlJQk4pfQ80nK5lYdUdGSr-F9Y3iGQdS3PM7l5HIMlYByjRVNvir2PQ9YRCxyrnXO1M8Hx147jQa59bWhrqcVv7vdoZph5gQP-I2qRLlHCAs298=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/FO8puyZOfwuRUES-ODIhe9HvvsS1SGmyTtTL3bZOot_2tTQ4vV7Amr2hn-w9yz-P0PuNdSAUUEvHrTe0mCnQAGu4CDzxdGfTSyVdFplCZiJU-Oe3Zv1NN69noENFmGLvoHxngiU5MBA=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/SCxSDzLeZRqpUz1DbjwVB2Bd6bi-uphg48UuzLCoA6RCDzBEUppAAQTksb1WU0Wb9XPgR92ZvOHvP2ATB1xojzQippshbxZqqmFrjWfZZOSBFkXxXpK0NMQEvAB7Hk0cY2UTSYIeqW0=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/SBEXyVcL0M0p1bFc7EXb0KIfuFGhizsS5OuQ4yp79J_-bJq9OHyIxm7n7A6Jn1ZURFPrzS82R2dN0-70EAnT9GMZzEq6oKlZcsQ01HPkX24ppRkpSVGC_YP4u0j1DPBkyrww8q7wkOY=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/lK7Aa2R_4g39jbZRueAAfmU_dtG9kL9D9BaQBWUfH7-BdrYMu7qbc4cxAV1QGZyqV_knpc7ubyQ2NnGwmMAEWGulRMBibx3MxbBgLU7sYp0NZkSH_3KAtkrk-h8QGS_aNLjBqw2WtdQ=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/1hQsd7E2msWJAlqb914lcqeRzin65TJQ5F7ApvK5gyer-pxXl-JPJQtKihFHsVCdTVFDXaixrhEEJ37BsR8xMh0AuYKY9mRD8o70X9JMTcYyjCH2NTPueWennJVWogR5RLYiWXxMGsw=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/otckJVS40rOo1zLLzHL_j5MgL3YjvtyoLvcmXp6xYLfSSS9OcSTTYkl0ZT4e0A6AYcr3QCd5_OB0bpkyrwEkZi9Ixx_ySmVINI135mSLDmd3RbpY7rizcyACCpWZ0g6iJZjLpHlar_w=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/66PCDYIWBCUDMzHKfe4uOR4kdfDAiVYZEvGSyWMCn6n0CQfE_2bkDA6V-1HP6iAnZUxXVVMv9G16UNREXrIVA3JJXtQY8HnlnWmGKMEuicrbIgsrnp9C85kDB3jsUH89lGimEeU8fHk=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/_Ep8Ke5Nrxe0LSGP6nCIfw3f6xtyNiJuEqkwx-ppa2dJ_Cj2atHtNwMEQ-Sn0SkEg6uzmjSKRt2fSmEdhP73KI9lQwAyr35jvL6qtb2gKC9lmG_UVOMSOU4I2jhfKSHYocCKaN5FnNI=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/3YFjnXDnZfDPDIybMe2dcoKUOfJkDXOc0iWka4BbmOTuzg_s6yKkbEs0vrPWQvQv_J9AqOhw0jtLegtQJy09D3AKHuTRdsrCQ38C2H6TVNWAUNQlEa8agmG6la0COVXChb2yjhsbJiU=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/ljCwtU40dddVgzeFH0jHgoD2Qq1Vz8bg7RlwxjuwMS1IL5x3OWXWvJls5i6T99cMRxj0zH-nCJ5H0hAzjnzzMMZf_IRsbYDj9p5bhhdptwhd7Ymv4XbWnwmmgr6NKhKISeHkzh4_UIU=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/eQII6yrZ2lBlPITeaX7A4CRr4gRqanal7uXmmSGn1Z3ZHXEXAIDJZxUiQsi_9bOH2H0eU9uCyg-WVs2MQe-yJVfHOBCHsD2ngKsNYHrfOMzHJu3MeGoss6BCyMx1U9rAJmnS2mywccM=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/wyubqY7NfGn7wLPtSIntuvLuuBg5yDwGs_V_HcONPIj1jyGu4wdKgqWwmTtxwhiGxkFDLOZeAReTQw2tSNR0OJ6D_MMRvju9SjuRD4evPD5WEVcoJv8-XmIYedlqMwgDlekX-CfaNmM=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/qanUEXc6t1Ouleub8SsPNgXTXPZySIAmUbZYRnNO1NUnDxf-vLVNHnvYdZQDwzgwMwukW8acoXBrbZ4bU3F4IL8_pgGlbaF-GwcUnNKuWt4nO0EQqfmBs2QME8Zz6DysJh35zSks5vo=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/Rmi_sG7dsuvoqoMVZgm2AocRkrpfMCqoWbgKQij5xCDgEJV8vssjBPkHQx1Jv1_vJQ0-U0rearN_jYyj2pNmUWADtJxPLKkjX5wZFdfIQn9DLZ7itD8cPntUnIv9ZakIgNPUUBWNT44=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/dovHtWVlkNWUu3sTRq0xKl1XO3757glDMHo0ws3ydi7_jqk5u-1MoKbimxBIJ32mf1cIprA6iYTGgYDRM1MkRP041iXL-48dGGKxiYp2t3O5PMblZDcYe9oXGKLfpc4OIN83dVvQUFM=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/Vz1q0bgLnDhkPvVJu7KIGFl2dCwniYX4zPi2kA3qmFsvHnpL3ifLzoXAhaxKqjkCbwdOqfZDtZP1F9PduvfiQhIgLtXvKqoKMW54CfCUlJAmSL7AbCnJ5NS8TihBaMGJFmL_RFodQm4=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/x0JQCLRZECeA5YGl9Es0XHGmgRZ5SWknkSXHpp-0Lj3VMYGmwI32uDgGdslkMavYsrxJQjg4HAc-NYvECPft3IQDj3PHHAzT9LUJ1su0Ncd7s1jUqglmpYh63-zgHbHw_-RDJFXmfag=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/YLU6bmXQuzYS5QO1z0HGwOatSJvIrfVZQhXvUnpYZgt2ZdpWgJrN_VGflLqTEnEVwPJN86Q3TYoEkiJj5YhiB_PQEOuB9xS391EyeLP4LRe2u_v9n3njSMTACRcteKWDidaICLqi8yo=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/QYBzWFy_MnEvTBpG2jYIPZXcK_jjOsZoR32xwLIe2l1usOgScOvh4sb2yYa0jFw0YuyORJ_00rsEb6LatvMth9YCehZnwa6jqML4W5S_EsYX_oGq6uxtUBodqmaCwy6-NCS7novRDSg=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/F1nW6nYbZRaBSPresbJtcUf7C7vyP1tkcizNjE7Y-21HB8q363j87-6oJYte_HzMbNewtQgcYO847jXJtIruliUF0oGjGULvETpik8UP0jBZiV61S-tHWjwRsDTYarFMKiaEdkSe-no=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/nyMh7LI2LLvT857UeBRyatBHL8TeFBkTXe56JP174Qihgj1d1eTfhsMqRtgFKKvwfsBHVZL55PX5Q55biuL1kfrf2Cbl5cd66JuhhWeO8kSYPzJesUHBIv-YRwxyUY-ec-V9F6mFnV4=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/cxoYNl6qdkFlLlhue1yPKtaAq6iGQQRZfELZgqyPOVNXb1C5mvIGohC9cDJADeVHuwy7V6vYsdJrJbcrDdDCNSYYy3bJygOxQhwcx9yaTQA2jc2QO2_SFI1z6qVl1HcGCFOU4yfFLcM=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/mYBfgQkDQX4QuqSlSZRh8SSwXWn95qCZOxI5YU_CBmeY-kFTE1mOPFK4o3uTlhQ451PZYHN6v1s0G1g5RpW7Tc5mfKjE7P4avJG_amUUyHUq87px77aCmdnq7-VcW_vD4mocIaF7Z-g=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/0gQ1lpXwx9CTX_gZPmkQme7j4QUxJG1HTJgW_lTnmCZTo_BCLMHjv5JdBF6Joy_TXYIt5KeK-WAFvU7V43o2HjvCL3nyYAuvVMm-WccOtR__h8LTDw6MUcZxgSwybettsseX9VElbzc=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/kQBj9YXFK8z9XKTeT0fmsBLQFvpqPQ0C0dU-Pkygj0hSKba2OtbtUD64ANhwGkk4t8GDL_Vg-AEG7S0fh63fR_tjCy291sqh8X6eWdUMLiPm2ijIIfRCTMRqXF7PqYDU_XItfXPQYew=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/aZiONYK8JmqnmsUPQF0GPZRjljKrq8jpyLnixTK5D3ebBPkuQwMUZMceQcBbbcPTXo58jUGJ_n-D_1P-8UYKMw8X_z5_DXKLOkdGj0rXTe2nDOblmbQedC7peiPrbS7wMTucLVs4LRI=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/-m64suosN3O9GbodGHuphoNpCr0gfxccC_io1R8AKKgl1ioOH1W7lBTYyduHXe55e5dqjJ-9GTZJDQ5DT9c038t23zKxgaDrlDYEYgdSng9i_SghNiaA29aKYCIp0w5C7DYtCKLu3Wc=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/x0OvVvhBYlWSw9KpFEAfFsiEcBctXbsmtV5NiAmtc9VTmaaU1RM4-sOVRA7Ie-ULdcLvCli88XqoH1Q1XpNHHj7KH4ucnmLBmT-8-ckPPe0e-B5LCeOGQN2RQWeQaS4zoqCUTc2rj5k=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/6h_T1VIZjVXEylvZRygBksX6-GqzK-OCo0vi3raIYuefamCKrbagmynCA7BpVz3CCr4FMU38myd6nf3smIerzenfZypQfWJ1NxCD4hd4sX_F8BWyprTgJDsjLZYXfKOCshZiTh9sTYQ=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/vx5peMA30m7NOqTZzD0iDAeAV1TOQgIeYB6vPxQtshftEltcBPo4QTvtjXFihunJaguDKARkhrVpCgVTRM1jx-DiBA07dhI0TvyEA2l-dcgK7Rj6tD1rxpjZ11ElXIsx6-PyW1xmWWs=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/a8AO_4eKqJ0WDyJDKOimfS5GgyOdXUIwgk_DU2gP8Y7fb7MTV0pJBFDsefgSBVo2FUcwg0p2iOHuiyeEbGoSa5IJZ45DEwc7fovpwhXiSCqnyi_SK6BOZ7UzTwIAY328yr27BqLkWMs=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/-uErHlqgQnX1BeQYfCiSTGpDqmXyN_p9oomDuoimaLdI-cmJzjxp_SzXIaynFjf1_rNt_LNJGP90cXU6WZqbqkIYSBKY_7wKR__iGGDgbYN0A_ZJc7OVKeWegRPf9LGp5X1ztyQW-tg=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/Oq-b3ESNhr2555uPxGwYNaHUDv7x5akoaHz7ilBdhTet2lk-d-rF66QU7JnzMiltZe4c64UxICJHum01T27mmIEXyO1WJX_slf6crNuiv9AuWmBm7tKLEwh8t-Ip7Qi8G0rcu0ZDbLI=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/gGXBoNGWx3mBaWxpUK4I-2CVNJRtMbQUV464viBrMkWnNdeFvg5vB_x46ZU2FOOU7dSh1dxC-sGFqAxPOKiukvlrxHskAzOWmMSNrpYTsWlOmVEI1JgDhoENYsEgh57ZcI7BQ5LOEKg=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/Vd75YQ17qv6NiZD6EEL4OZa4r2hnXCUGf6ahWV-vKnLu0uPBfZBKfBujdoucy9EdYUmgAg_w0SEwW4lmLSSRor4l9QBJOs7PSASt7mm87p5s8zR0wJC4WaclB6p-YkGZ7YqmYaiWdVk=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/FiTD24qSdnB61vbvv-9p6e03iVPLzZ-286oRFT_mU-i0ovpLdpv2osrI8oMb8t22AhWqm9VRGRtO3bK-HI--OtNDs87rEmJvNXkknvaZqKRIJRC4tHWUI8O2t2bEfmRzU0MN2AoQzPM=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/RDmEqaeuY-WkbFMWWXbe0St4p3Vvro2-97lwCfSeuN8ZaPBpmyp4owWqZxw8ihve--ik_rvZh5yILKyiA5HAVQEku3aHJlC15fespO67lrAwpgimZUFKasiE3aCawKua10M3LIn-z_U=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/cMC6sWe-5WpqYwdpI1FUSbwZDtSuyWWaRmYwi5NNUW6EuecvLn942q_VJYn60QIWyvXkm9MmfCxL8xGDcibCQeo8B2FgNdS3uS0Y002uhId6LGqW2Ubk9BedQGi8l-fb2OUSEEP1dy4=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/-B5QU_KXCXnWAFianm3IShBtF6ARur52yktZPuura6qVWWhLhJGeS9Qn92gBP44phssZN5MhAUe_p02CDmtWIpu4YbxR4jypCfS8BTap7SNAMMiioEtSfLZsRAZLb_EVRMi2gAoojpo=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/4Awh-tT2wXr_K9XcKg8zDu0U7sRbn5riYp4VNeGoneUJrXtMFKx-dWzbFObbpBURRBb0ROV5bmfoGEttzAfSdd-CuH32gLkSuU_o-kneRZK-2lNknvjW2MCxAmdip9V6wK0lgwdT48s=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/FJ6nK_9kTQZKA98RQieI3bMD6fgSyVLDhoCpyUAiHp3VFNAfmbSqH3PjAxRdiE8oaxEJQrICVKume3PlgLyy-LfnqntoNXwRsOwQGjpjTZS37ivwssxY6FaGBrYwhA9nBJya00NwFPQ=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/MuWdNoV3iZ9CyELSepWrbDNmWLv_amB3AREozp4OZwTPDnRFdiCthSR7daH38UQwF97KHSjtNeOlskUZyFGBnc8CL6dR0uaPI9OuIQGVl7QLgbOxT0Oo8jpmVE67t61_x5wgm25rVF4=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/d7x4j7DJ3N6YCdZi9V4wc3-EUvUCnV9i2GkgFZjT3zP3FjSYNj2UQ3nKbs3t09dUvLznIzvhAjlHP0i5n6L6sMHBlZ_9xHCfYA3ftjH-NALtepipKgHU90nLl0IwlbkIhPymHkzHqLs=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/gTPBss4pzkdqDUcVFKNnJgt9rmYu9J9bge_QCUELg6m1DO2LdiG5D5RB_JpVH-ZEjWKHIUgd6C6xvUdTwjm5xhK0cIRy3FrmtHZhJ5kBQ3dWjVEsPT_SY87dsCaEacbaDoWPR6UUFRs=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/lWM330CJnfgbHXlUhsVfAtkjFmV1vc6xx12YDft6J6I_2r-5nj3gjYDKBJKZmRqggWMm4qWbBxJz45zZ-VnXPEdEZfHJKNfvmGi9y9644_nCzS-mPpk8cFuCkry2ujRNpu9a-ekh-Sw=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/NdpQXRyJvLHXIMyngS6obxo2lpKvhznC2q8QOUGS9CkTncAKPjYLGrt5vA153RLc09_WPINk0MbnnJbnnPNiRArH5lILBuUJimF2zh1_B6AcmEiBfL6r_xSnqqSJV6rHboubyy8J4i8=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/6cOjcrDwUZWAJCTh07wEPPc2rdS97lhJMpUMWiBip8UecBWQwBaP4IVd5mDvu4xgPjjeU2fdl1GkznI5Ms2oe319_uklNyGSpeizKLb9hK4HT5GMjaOqsNYMwtWhwHGKbI7DdgKIoSw=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/7xAP6TXG4oK0Fr3iJ6bQvU8nQ_oYRcm-MNhPbgsdN0iMU4Xs_749-4tYkQ_FxjNaSO7sy5UAIRMmd_cu66dE2gHGagu0IZ6QMn8zuy8IGviBPVpnj1sKQnm2rWE2aaKDsvUDK0nEGUc=w1920-h1080" src="" alt="" />
+      <img data-src="https://lh3.googleusercontent.com/EwyyNIt3kCGgyqmXjwsK-Oa-_cjdyJKQo818ZNIJzlUp5z9B7ltju1Z4ZZy7bOJ6L46b18web7Nf3ynAIUt-onbm-KbwbHRqg52MgOIqBaO8gukTqaIV110oJL6xpEVqLOvqNHuPpWg=w1920-h1080" src="" alt="" />
+    </div>
+
+
+
+    <div>
+      <br>
+      <p>View our photo shoot in Montmartre at the Sacré-Cœur, by <a href="https://www.missparisphoto.com/">Krystal Kenny</a>.</p>
     </div>
       
     <div class="pa-embed-player" style="width:100%; height:480px; display:none;"
